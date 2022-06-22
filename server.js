3fcc67d37c0abbb8c0f235f4323f8b1a4133cf2f
Jhony Albayero <jhonyalbayero@gmail.com>
2022-06-22 19:52:11 -0400

    test

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = express()
 const corse = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dontenv').config()
+
+let db,
+    dbConnectonString = process.env.DB_STRING,
+    dbName = '',
+    collection
