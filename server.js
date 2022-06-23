24394028aa7f2268964e42e3d33decd6726fb192
Jhony Albayero <jhonyalbayero@gmail.com>
2022-06-22 21:02:10 -0400

    setup middleware

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ MongoClient.connect(dbConnectonString)
         collection = db.collection('test')
     })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`SERVER RUNNING ON PORT 8000`)
 })
